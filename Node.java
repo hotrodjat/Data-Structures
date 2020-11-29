18041eb499e2c7aa3cbecf05e1807aaf16464f56
Quocasaurusss <51280185+Quocasaurusss@users.noreply.github.com>
2020-11-29 00:30:13 -0600

    Fixed addNode method
    I added in a setLeftChild and setRightChild method.

--- Node.java
+++ Node.java
@@ -1,12 +1,3 @@
-
-
-
-/**
- * Write a description of class Node here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Node
 {
     // instance variables - replace the example below with your own
@@ -37,8 +28,16 @@ public class Node
         return leftChild;
     }
     
+    protected void setLeftChild(Node left){
+        leftChild = left;
+    }
+    
     protected Node getRightChild() {
         return rightChild;
+    }
+    
+    protected void setRightChild(Node right) {
+        rightChild = right;
     }
     
     protected String getData(){
