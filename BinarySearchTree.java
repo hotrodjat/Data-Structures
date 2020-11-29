11a8c566f33b58b581c5c8ae02d06470bfccfd4d
Quocasaurusss <51280185+Quocasaurusss@users.noreply.github.com>
2020-11-29 15:40:06 -0600

    Added removeNode method.

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -38,6 +38,72 @@ public class BinarySearchTree {
         addNode(root, data);
     }
     
+    private Node removeNode(Node localRoot, String data) {
+        if (localRoot == null) {
+            /*
+             * This takes care of the case when the desired node is not in the tree.
+             */
+            return null;
+        }
+        if (data.equals(localRoot.getData())) {
+            /*
+             * Removed node has no children.
+             */
+            if (localRoot.getRightChild() == null && localRoot.getLeftChild() == null) {
+                if (localRoot == root) {
+                    root = null;
+                }
+                return null;
+            }
+            /*
+             * Removed node has only left child.
+             */
+            else if (localRoot.getRightChild() == null) {
+                if (localRoot == root) {
+                    root = localRoot.getLeftChild();
+                }
+                return localRoot.getLeftChild();
+            }
+            /*
+             * Removed node has only right child.
+             */
+            else if (localRoot.getLeftChild() == null) {
+                if (localRoot == root) {
+                    root = localRoot.getRightChild();
+                }
+                return localRoot.getRightChild();
+            }
+            /*
+             * Removed node has two children.
+             */
+            else {
+                Node current = localRoot.getRightChild();
+                while (current.getLeftChild() != null) {
+                    current = current.getLeftChild();
+                }
+                removeNode(root, current.getData());
+                current.setRightChild(localRoot.getRightChild());
+                current.setLeftChild(localRoot.getLeftChild());
+                if (localRoot == root) {
+                    root = current;
+                }
+                return current;
+            }
+        }
+        else if (data.compareTo(localRoot.getData()) > 0) {
+            localRoot.setRightChild(removeNode(localRoot.getRightChild(), data));
+            return localRoot;
+        }
+        else {
+            localRoot.setLeftChild(removeNode(localRoot.getLeftChild(), data));
+            return localRoot;
+        }
+    }
+    
+    public void removeNode(String data){
+        removeNode(root, data);
+    }
+    
     public Node getNode(Node localRoot, String data) {
     	if (localRoot == null) {
     		return new Node();
