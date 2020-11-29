6996b6168380352db5cb6e20221400ac60e0694e
Mang0la <56270888+Mang0la@users.noreply.github.com>
2020-11-29 01:52:07 -0600

    Update BinarySearchTree.java
    Implemented getNode, contains, (in/pre/post)orderTraversal, get(Size/Root) functions.

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -9,11 +9,13 @@ public class BinarySearchTree {
     private void addNode(Node localRoot, String data) {
         if (root == null) {
             root = new Node(data);
+            size++;
         }
         else {
             if (data.compareTo(localRoot.getData()) > 0) {
                 if (localRoot.getRightChild() == null) {
                     localRoot.setRightChild(new Node(data));
+                    size++;
                 }
                 else {
                     addNode(localRoot.getRightChild(), data);
@@ -22,6 +24,7 @@ public class BinarySearchTree {
             else if (data.compareTo(localRoot.getData()) < 0) {
                 if (localRoot.getLeftChild() == null) {
                     localRoot.setLeftChild(new Node(data));
+                    size++;
                 }
                 else {
                     addNode(localRoot.getLeftChild(), data);
@@ -30,14 +33,110 @@ public class BinarySearchTree {
         }
         
     }
-    private void addNode(String data) {
+    
+    public void addNode(String data) {
         addNode(root, data);
     }
-    public static void main() {
+    
+    public Node getNode(Node localRoot, String data) {
+    	if (localRoot == null) {
+    		return new Node();
+    	}
+    	if (data.compareTo(localRoot.getData()) == 0) {
+    		return localRoot;
+    	}
+    	else if (data.compareTo(localRoot.getData()) < 0) {
+    		return getNode(localRoot.getLeftChild(), data);
+    	}
+    	else {
+    		return getNode(localRoot.getRightChild(), data);
+    	}
+    	
+    }
+    
+    public Node getNode(String data) {
+        return getNode(root, data);
+    }
+    
+    public boolean contains(Node localRoot, String data) {
+    	if (localRoot == null) {
+    		return false;
+    	}
+    	if (data.compareTo(localRoot.getData()) == 0) {
+    		return true;
+    	}
+    	else if (data.compareTo(localRoot.getData()) < 0) {
+    		return contains(localRoot.getLeftChild(), data);
+    	}
+    	else {
+    		return contains(localRoot.getRightChild(), data);
+    	}
+    }
+    
+    public boolean contains(String data) {
+    	return contains(root, data);
+    }
+    
+    public void inorderTraversal(Node localRoot) {
+    	if (localRoot == null) {
+    		return;
+    	}
+    	else {
+    		inorderTraversal(localRoot.getLeftChild());
+    		System.out.println(localRoot.getData());
+    		inorderTraversal(localRoot.getRightChild());
+    		
+    	}
+    }
+    
+    public void preorderTraversal(Node localRoot) {
+    	if (localRoot == null) {
+    		return;
+    	}
+    	else {
+    		System.out.println(localRoot.getData());
+    		preorderTraversal(localRoot.getLeftChild());
+    		preorderTraversal(localRoot.getRightChild());
+    		
+    	}
+    }
+    
+    public void postorderTraversal(Node localRoot) {
+    	if (localRoot == null) {
+    		return;
+    	}
+    	else {
+    		postorderTraversal(localRoot.getLeftChild());
+    		postorderTraversal(localRoot.getRightChild());
+    		System.out.println(localRoot.getData());
+    	}
+    }
+    
+    public int getSize() {
+    	return size;
+    }
+    
+    public Node getRoot() {
+    	return root;
+    }
+    
+    public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
         tree.addNode("tomato");
         tree.addNode("zebra");
         tree.addNode("apple");
-        System.out.println("Tree root: " + tree.root.getLeftChild().getData());
+        tree.addNode(" urmom");
+        tree.addNode("$&!@#!**%!(%(");
+        tree.addNode("BdsaBbbVVsas");
+        System.out.println(tree.contains("urmom"));
+        System.out.println(tree.contains("$&!@#!**%!(%("));
+        System.out.println(tree.getNode("tomato").getData());
+        System.out.println(tree.getNode("zeb").getData());
+        System.out.println(tree.getSize());
+        System.out.println(tree.getRoot().getData());
+        tree.inorderTraversal(tree.root);
+        
+        
+        System.out.println("Tree root: " + tree.root.getData());
     }
 }
