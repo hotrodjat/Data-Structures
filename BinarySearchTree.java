1ead55a2251b1b48ef3465e66ef0a4467c139689
Quocasaurusss <51280185+Quocasaurusss@users.noreply.github.com>
2020-11-29 00:28:26 -0600

    Fixed addNode method.

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,50 +1,43 @@
-
-
-
 public class BinarySearchTree {
-	private Node root;
-	private int size;
-	
-	public BinarySearchTree() {
-		root = null;
-		size = 0;
-	}
-	
-	private void addNode(Node localRoot, String data) {
-		if (localRoot == null) {
-			localRoot = new Node(data);
-		}
-		else if (data.compareTo(localRoot.getData()) > 0) {
-			addNode(localRoot.getRightChild(), data);
-		}
-		else if (data.compareTo(localRoot.getData()) < 0) {
-			addNode(localRoot.getLeftChild(), data);
-		}
-		else {
-			localRoot.incrementCount();
-		}
-		
-	}
-	
-	public void addNode(String data) {
-		addNode(root, data);
-	}
-	
-	
-	public static void main(String args[]) {
-		BinarySearchTree tree = new BinarySearchTree();
-		tree.addNode("Quoc is gay.");
-		try {
-			System.out.print(tree.root.getData());
-		}
-		catch (Exception e) {
-			System.out.print("Thomas sucks at programming.");
-		}
-		
-	}
-	
-//	public Node getNode(locaRoot, data) {
-//		if ()
-//	}
-	
+    private Node root;
+    private int size;
+        
+    public BinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+    private void addNode(Node localRoot, String data) {
+        if (root == null) {
+            root = new Node(data);
+        }
+        else {
+            if (data.compareTo(localRoot.getData()) > 0) {
+                if (localRoot.getRightChild() == null) {
+                    localRoot.setRightChild(new Node(data));
+                }
+                else {
+                    addNode(localRoot.getRightChild(), data);
+                }
+            }
+            else if (data.compareTo(localRoot.getData()) < 0) {
+                if (localRoot.getLeftChild() == null) {
+                    localRoot.setLeftChild(new Node(data));
+                }
+                else {
+                    addNode(localRoot.getLeftChild(), data);
+                }
+            }
+        }
+        
+    }
+    private void addNode(String data) {
+        addNode(root, data);
+    }
+    public static void main() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.addNode("tomato");
+        tree.addNode("zebra");
+        tree.addNode("apple");
+        System.out.println("Tree root: " + tree.root.getLeftChild().getData());
+    }
 }
